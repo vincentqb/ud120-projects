76663c6798820e083bb35328bc06b4aa2e7da347
Vincent QB <vincentqb@users.noreply.github.com>
2016-06-02 15:12:52 -0400

    print number of features.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,9 +25,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+print "Number of features: {}".format(features_train.shape[1])
+
 from sklearn.tree import DecisionTreeClassifier
+
 clf = DecisionTreeClassifier(min_samples_split = 40)
 clf.fit(features_train, labels_train)
+
 pred = clf.predict(features_test)
 
 from sklearn.metrics import accuracy_score
