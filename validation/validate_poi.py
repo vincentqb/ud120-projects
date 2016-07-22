5af721370c9f1ede5f72493b5180d2499c60f756
Vincent QB <vincentqb@gmail.com>
2016-07-22 14:04:02 -0400

    exercise done.

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -29,4 +29,15 @@ labels, features = targetFeatureSplit(data)
 
 ### it's all yours from here forward!  
 
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
 
+from sklearn import cross_validation
+features_train, features_test, labels_train, labels_test = \
+    cross_validation.train_test_split(features, labels, test_size = 0.3, random_state = 42)
+
+clf.fit(features_train, labels_train)
+predict = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+print(accuracy_score(predict, labels_test))
